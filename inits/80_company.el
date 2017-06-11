960e3b96570b37196dd25e999b4b28b903891f8d
Koji Matsuda <kjmatsuda1985@gmail.com>
2017-06-12 02:42:24 +0900

    add settings for certain environment

---
+++ inits/80_company.el
@@ -0,0 +1,24 @@
+(if (string= system-name "KHW-CP0075")
+    (progn
+      ;; 日本語フォント(社用)
+      (cond
+       (window-system
+        (set-default-font "Ricty Diminished-12")
+        )
+       )
+
+      (setq org-agenda-files (list "~/WORK/org/"))
+      (setq org-default-notes-file "~/Dropbox/org/agenda/agenda.org")
+
+      ;; ローカルに ical をエクスポート
+      ;; http://d.hatena.ne.jp/t0m0_tomo/20100103/1262537012
+      (setq org-combined-agenda-icalendar-file "~/WORK/calendar/org.ics")
+
+      (defun org-push-daily-my ()
+        (interactive)
+        (start-process "org-to-ics" "*org-to-ics*" "emacs" "--script" "~/org-to-ics.el"))
+      (define-key global-map [f12] 'org-push-daily-my)
+
+      ;; org ファイルを ics に変換
+      (org-push-daily-my)
+      ))
