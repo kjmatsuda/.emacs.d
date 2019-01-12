f895b01886ef8b36203bb83c7c3fd25aa5fda816
Koji Matsuda <kjmatsuda1985@gmail.com>
2019-01-12 19:00:44 +0900

    introduce web-mode

--- inits/20_web-mode.el
+++ inits/20_web-mode.el
@@ -1,13 +1,24 @@
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-engines-alist
+'(("php"    . "\\.phtml\\'")
+  ("blade"  . "\\.blade\\.")))
+
+(setq web-mode-auto-close-style 2)
+(setq web-mode-tag-auto-close-style t)
+(setq web-mode-enable-auto-pairing t)
+
 ;; html や css の編集に便利な emmet-mode
 (require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 (setq emmet-move-cursor-between-quotes t) ;; default nil
 
-;; emmet-mode を入れたので zencoding-mode はコメントアウトする
-;; ;; Zen Coding Mode
-;; (require 'zencoding-mode)
-;; (add-hook 'sgml-mode-hook 'zencoding-mode)
-;; (define-key zencoding-mode-keymap (kbd "C-z") 'zencoding-expand-yas)
-;; (define-key zencoding-mode-keymap (kbd "<C-return>") nil)
-;; (define-key zencoding-mode-keymap (kbd "<S-return>") 'zencoding-expand-line)
