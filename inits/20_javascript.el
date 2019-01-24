2f73eca589a43df03e2c2f3f716b0660f4a243b0
Koji Matsuda <kjmatsuda1985@gmail.com>
2019-01-24 16:01:45 +0900

    introduce indium

--- inits/20_javascript.el
+++ inits/20_javascript.el
@@ -6,15 +6,18 @@
     (lambda ()
         (tern-mode t)))
 
+(when (require 'indium nil t)
+  (add-hook 'js-mode-hook #'indium-interaction-mode))
+
 (eval-after-load 'tern
     '(progn
         (require 'tern-auto-complete)
         (tern-ac-setup)))
 
-
-(if (is-termux)
-    (progn
-      (add-hook 'js2-mode-hook #'electric-spacing-mode)))
+;; key-comboが有効だとjavascript-debuggerが起動した際のミニバッファで
+;; 何を入力してもundefinedになる
+(add-hook 'js2-mode-hook (lambda () (key-combo-mode -1)))
+(add-hook 'js2-mode-hook #'electric-spacing-mode)
 
 
 
