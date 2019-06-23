035a0f1c7a5d8cde0386c3fe994067091609556d
Koji Matsuda <kjmatsuda1985@gmail.com>
2019-06-23 22:00:12 +0900

    introduce image+

--- inits/03_visual.el
+++ inits/03_visual.el
@@ -257,3 +257,18 @@
     )
   )
 (global-set-key (kbd "M-R") 'highlight-symbol-query-replace)
+
+;; image+
+(eval-after-load 'image+
+      `(when (require 'hydra nil t)
+         (defhydra imagex-sticky-binding (global-map "C-x C-l")
+           "Manipulating Image"
+           ("+" imagex-sticky-zoom-in "zoom in")
+           ("-" imagex-sticky-zoom-out "zoom out")
+           ("M" imagex-sticky-maximize "maximize")
+           ("O" imagex-sticky-restore-original "restore original")
+           ("S" imagex-sticky-save-image "save file")
+           ("r" imagex-sticky-rotate-right "rotate right")
+           ("l" imagex-sticky-rotate-left "rotate left"))))
+
+(eval-after-load 'image+ '(imagex-auto-adjust-mode 1))
