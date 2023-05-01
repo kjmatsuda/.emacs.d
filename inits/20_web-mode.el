3a3179f77f763d8b042c726f8e485bc6ce9f7e5b
Koji Matsuda <kjmatsuda1985@gmail.com>
2023-05-01 16:32:29 +0900

    emmet-mode を改めて整理する

--- inits/20_web-mode.el
+++ inits/20_web-mode.el
@@ -20,10 +20,29 @@
 ;; html や css の編集に便利な emmet-mode
 (use-package emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'html-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 (add-hook 'web-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 (setq emmet-move-cursor-between-quotes t) ;; default nil
 
+(add-hook 'web-mode-hook
+          '(lambda ()
+             ;; HTML を編集するときに company-mode の補完が邪魔なのでオフにする
+             (company-mode -1)
+             )
+          )
+
 (add-hook 'css-mode-hook 'ac-emmet-css-setup)
 (add-hook 'sgml-mode-hook 'ac-emmet-html-setup)
+(add-hook 'html-mode-hook 'ac-emmet-html-setup)
 (add-hook 'web-mode-hook 'ac-emmet-html-setup)
+
+(add-hook 'sgml-mode-hook 'auto-complete-mode)
+(add-hook 'html-mode-hook 'auto-complete-mode)
+(add-hook 'css-mode-hook  'auto-complete-mode)
+(add-hook 'web-mode-hook  'auto-complete-mode)
+
+(add-hook 'auto-complete-mode-hook
+          (lambda ()
+            (define-key ac-completing-map "\C-n" 'ac-next)
+            (define-key ac-completing-map "\C-p" 'ac-previous)))
