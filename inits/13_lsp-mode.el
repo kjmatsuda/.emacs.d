51b725af0cc173c869cb21d92411156a9f66fefd
Koji Matsuda <kjmatsuda1985@gmail.com>
2024-06-09 10:48:25 +0900

    test commit

--- inits/13_lsp-mode.el
+++ inits/13_lsp-mode.el
@@ -38,6 +38,7 @@
       (define-key lsp-mode-map (kbd "C-M-c") 'lsp-treemacs-call-hierarchy)
       )
   ;; key bindings
+  ;; TODO テストコミット。あとで消す
   (with-eval-after-load 'lsp-treemacs
       (define-key lsp-mode-map (kbd "M-c") 'lsp-treemacs-call-hierarchy)
     )
